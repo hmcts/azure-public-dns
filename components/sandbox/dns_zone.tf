e10311059e0a3601942981446feb82c1e37bb84e
Tim Jacomb <tim.jacomb@hmcts.net>
2019-12-06 12:35:28 +0000

    Add sandbox IDs

--- components/sandbox/dns_zone.tf
+++ components/sandbox/dns_zone.tf
@@ -3,7 +3,7 @@ data "azurerm_resource_group" "main" {
 }
 
 resource "azurerm_dns_zone" "zone" {
-  name                = "ithc.platform.hmcts.net"
+  name                = "sandbox.platform.hmcts.net"
   resource_group_name = data.azurerm_resource_group.main.name
 }
 // # azurerm_dns_ns_record.this-0:
---
+++ components/sandbox/terraform.tfvars
@@ -0,0 +1,4 @@
+resource_group_name = "reform-cft-mgmt"
+
+recordsets = [
+]
