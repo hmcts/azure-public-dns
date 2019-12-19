c8a2ef2335c53dfa87f91737d31a3734a19d9e62
Piyush Kumar <piyush.m82@gmail.com>
2019-12-19 11:13:27 +0000

    pertest dns apply (#8)
    * pertest dns apply
    
    * pertest dns apply

---
+++ components/perftest/variables.tf
@@ -0,0 +1,7 @@
+variable recordsets {}
+
+variable "resource_group_name" {
+  type        = string
+  description = "The name of the resource group that contains the DNS zone where the records will be added."
+  default     = "reformmgmtrg"
+}
