56debaf53c6414654d90bb4a15c13bdecc709b6f
Piyush Kumar <piyush.kumar@hmcts.net>
2020-01-15 22:18:19 +0000

    sutter config enablement

--- modules/azure-public-dns/cname_record.tf
+++ modules/azure-public-dns/cname_record.tf
@@ -1,3 +1,7 @@
+locals {
+  shutter_domain = "azureedge.net"
+}
+
 resource "azurerm_dns_cname_record" "this" {
   count = length(var.cname_records)
 
@@ -6,5 +10,6 @@ resource "azurerm_dns_cname_record" "this" {
 
   name   = var.cname_records[count.index].name
   ttl    = var.cname_records[count.index].ttl
-  record = var.cname_records[count.index].record
+  # record = var.cname_records[count.index].shutter == true ? "hmcts-${replace(split(".", var.cname_records[count.index].name)[length(split(".", var.cname_records[count.index].name))-1], "www", split(".", var.zone_name)[0])}-shutter-${var.env}.${local.shutter_domain}" : var.cname_records[count.index].record
+  record = var.cname_records[count.index].shutter == true ? "hmcts-${split(".", replace(join(".",[var.cname_records[count.index].name, var.zone_name]), "www.", ""))[0]}-shutter-${var.env}.${local.shutter_domain}" : var.cname_records[count.index].record
 }
