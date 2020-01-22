33e4dc7616c2879c094b8093b0481502351dc19c
Piyush Kumar <piyush.kumar@hmcts.net>
2020-01-22 14:05:53 +0000

    add code for apply divorce zone

---
+++ components/prod/apply-divorce.tf
@@ -0,0 +1,7 @@
+module "apply-divorce" {
+  source              = "../../modules/azure-public-dns/"
+  cname_records       = yamldecode(data.local_file.apply-divorce-config.content).cname
+  ns_recordsets       = yamldecode(data.local_file.apply-divorce-config.content).ns
+  zone_name           = azurerm_dns_zone.apply-divorce.name
+  resource_group_name = data.azurerm_resource_group.main.name
+}
