e28c6262df9e4ad86c5e2eb229cb45df3f900031
Piyush Kumar <piyush.m82@gmail.com>
2020-01-22 15:49:55 +0000

    apply-for-probate zone (#17)

--- components/prod/data.tf
+++ components/prod/data.tf
@@ -1,3 +1,7 @@
 data "local_file" "apply-divorce-config" {
   filename = "${path.cwd}/../../environments/prod/apply-divorce-service-gov-uk.yml"
 }
+
+data "local_file" "apply-for-probate-config" {
+  filename = "${path.cwd}/../../environments/prod/apply-for-probate-service-gov-uk.yml"
+}
