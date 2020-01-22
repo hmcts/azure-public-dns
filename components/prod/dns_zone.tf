15fa65d3bda021bb27c34aa05f04e6fc535fbdd9
Piyush Kumar <piyush.kumar@hmcts.net>
2020-01-22 15:43:19 +0000

    apply-for-probate zone

--- components/prod/dns_zone.tf
+++ components/prod/dns_zone.tf
@@ -6,3 +6,8 @@ resource "azurerm_dns_zone" "apply-divorce" {
   name                = "apply-divorce.service.gov.uk"
   resource_group_name = data.azurerm_resource_group.main.name
 }
+
+resource "azurerm_dns_zone" "apply-for-probate" {
+  name                = "apply-for-probate.service.gov.uk"
+  resource_group_name = data.azurerm_resource_group.main.name
+}
