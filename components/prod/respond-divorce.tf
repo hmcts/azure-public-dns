531372e6cc29d54fd4e97097154fedb1a9f2be26
Piyush Kumar <piyush.kumar@hmcts.net>
2020-01-24 15:28:29 +0000

    respond-divorce

---
+++ components/prod/respond-divorce.tf
@@ -0,0 +1,7 @@
+module "respond-divorce" {
+  source              = "../../modules/azure-public-dns/"
+  cname_records       = yamldecode(data.local_file.respond-divorce-config.content).cname
+  ns_recordsets       = yamldecode(data.local_file.respond-divorce-config.content).ns
+  zone_name           = azurerm_dns_zone.respond-divorce.name
+  resource_group_name = data.azurerm_resource_group.main.name
+}
