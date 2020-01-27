e870816516e053e241beaeacce7ce44b88d9904b
Piyush Kumar <piyush.m82@gmail.com>
2020-01-27 15:00:43 +0000

    update (#35)

--- components/prod/hearings.tf
+++ components/prod/hearings.tf
@@ -2,7 +2,8 @@ module "hearings" {
   source              = "../../modules/azure-public-dns/"
   cname_records       = yamldecode(data.local_file.hearings-config.content).cname
   ns_recordsets       = yamldecode(data.local_file.hearings-config.content).ns
-  a_recordsets      = yamldecode(data.local_file.hearings-config.content).A
+  a_recordsets        = yamldecode(data.local_file.hearings-config.content).A
+  srv_recordsets      = yamldecode(data.local_file.hearings-config.content).srv
   zone_name           = azurerm_dns_zone.hearings.name
   resource_group_name = data.azurerm_resource_group.main.name
 }
