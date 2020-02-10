1a0ec538ad43520a63d69c2107db6a6d01fb4c25
Piyush Kumar <piyush.kumar@hmcts.net>
2020-02-10 13:49:27 +0000

    add cname to perftest

--- components/perftest/azure-public-dns.tf
+++ components/perftest/azure-public-dns.tf
@@ -1,6 +1,7 @@
 module "public-dns" {
   source              = "../../modules/azure-public-dns/"
   ns_recordsets       = yamldecode(data.local_file.configuration.content).ns
+  cname_records       = yamldecode(data.local_file.configuration.content).cname
   zone_name           = azurerm_dns_zone.zone.name
   resource_group_name = data.azurerm_resource_group.main.name
 }
