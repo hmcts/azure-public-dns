e82b81a7b72706ae146e0dcaf9e5ec8c2bcb22c3
Piyush Kumar <piyush.m82@gmail.com>
2020-05-29 17:29:47 +0100

    add jcm judiciary records (#192)
    Co-authored-by: Piyush Kumar <piyush.kumar@hmcts.net>

--- components/prod/data.tf
+++ components/prod/data.tf
@@ -41,3 +41,7 @@ data "local_file" "hearings-config" {
 data "local_file" "reform-hmcts-config" {
   filename = "${path.cwd}/../../environments/prod/reform-hmcts-net.yml"
 }
+
+data "local_file" "jcm-judiciary-config" {
+  filename = "${path.cwd}/../../environments/prod/jcm-judiciary-uk.yml"
+}
