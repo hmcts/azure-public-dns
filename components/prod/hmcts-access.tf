e87f039e7054b44373143e9ebcd59822e0f3ca91
Thomas Kimber <44808766+tkimber-hmcts@users.noreply.github.com>
2020-05-30 00:30:49 +0100

    Revert "add alias record for hmcts-access to front door (SIDAM 2.0) (#198)" (#200)
    This reverts commit adc12ea0294259b7e931ffd48e45a2588640874b.

--- components/prod/hmcts-access.tf
+++ components/prod/hmcts-access.tf
@@ -1,7 +1,6 @@
 module "hmcts-access" {
   source              = "../../modules/azure-public-dns/"
   cname_records       = yamldecode(data.local_file.hmcts-access-config.content).cname
-  alias_recordsets    = yamldecode(data.local_file.hmcts-access-config.content).alias
   zone_name           = azurerm_dns_zone.hmcts-access.name
   resource_group_name = data.azurerm_resource_group.main.name
   env                 = var.env
