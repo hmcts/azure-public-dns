ab77454f0c44bbb7438592a0bd5287a5a8986658
Sat <gaindas@hotmail.com>
2020-06-22 09:24:50 +0100

    Created config for HMCTS NLE

--- components/nle/variables.tf
+++ components/nle/variables.tf
@@ -1,5 +1,5 @@
 variable "resource_group_name" {
   type        = string
   description = "The name of the resource group that contains the DNS zone where the records will be added."
-  default     = "frontdoor-nle"
+  default     = "hmcts-frontdoor-nle"
 }
