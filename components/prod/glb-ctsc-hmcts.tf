fc1850b5f0eab592c8c9da60416e649595febda4
zc-hmcts <54357991+zc-hmcts@users.noreply.github.com>
2020-08-19 17:12:45 +0100

    removed bits from glb-ctsc-hmcts.tf

--- components/prod/glb-ctsc-hmcts.tf
+++ components/prod/glb-ctsc-hmcts.tf
@@ -1,7 +1,5 @@
 module "glb-ctsc-hmcts" {
   source              = "../../modules/azure-public-dns/"
-  cname_records       = yamldecode(data.local_file.glb-ctsc-hmcts-net-config.content).cname
-  a_recordsets        = yamldecode(data.local_file.glb-ctsc-hmcts-net-config.content).A
   zone_name           = azurerm_dns_zone.glb-ctsc-hmcts.name
   resource_group_name = data.azurerm_resource_group.main.name
   env                 = var.env
