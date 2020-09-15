6d5c5e11e57c9be96a4b13b7fcb13fb5709e1b30
zc-hmcts <54357991+zc-hmcts@users.noreply.github.com>
2020-09-15 10:22:43 +0100

    ammended

--- components/prod/ctsc-hmcts.tf
+++ components/prod/ctsc-hmcts.tf
@@ -1,7 +1,7 @@
 module "ctsc-hmcts" {
   source              = "../../modules/azure-public-dns/"
-  cname_records       = yamldecode(data.local_file.glb-ctsc-hmcts-net-config.content).cname
-  a_recordsets        = yamldecode(data.local_file.glb-ctsc-hmcts-net-config.content).A
+  cname_records       = yamldecode(data.local_file.ctsc-hmcts-net-config.content).cname
+  a_recordsets        = yamldecode(data.local_file.ctsc-hmcts-net-config.content).A
   zone_name           = azurerm_dns_zone.ctsc-hmcts.name
   resource_group_name = data.azurerm_resource_group.main.name
   env                 = var.env
