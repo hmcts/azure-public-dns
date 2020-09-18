96499569497eda5a6869b08546210a332ac18e98
zc-hmcts <54357991+zc-hmcts@users.noreply.github.com>
2020-09-18 10:12:55 +0100

    ammended

--- components/prod/hmcts-webchat.tf
+++ components/prod/hmcts-webchat.tf
@@ -1,6 +1,5 @@
 module "hmcts-webchat" {
   source              = "../../modules/azure-public-dns/"
-  cname_records       = yamldecode(data.local_file.hmcts-webchat-config.content).cname
   zone_name           = azurerm_dns_zone.hmcts-webchat.name
   resource_group_name = data.azurerm_resource_group.main.name
   env                 = var.env
