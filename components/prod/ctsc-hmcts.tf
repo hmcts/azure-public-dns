e2ed11258a153c997afd86e8e13347368a8890e2
rahman124 <56344599+rahman124@users.noreply.github.com>
2020-09-28 12:52:02 +0100

    add ctsc-hmcts a records (#298)

--- components/prod/ctsc-hmcts.tf
+++ components/prod/ctsc-hmcts.tf
@@ -1,6 +1,7 @@
 module "ctsc-hmcts" {
   source              = "../../modules/azure-public-dns/"
   cname_records       = yamldecode(data.local_file.ctsc-hmcts-config.content).cname
+  a_recordsets        = yamldecode(data.local_file.ctsc-hmcts-config.content).A
   zone_name           = azurerm_dns_zone.ctsc-hmcts.name
   resource_group_name = data.azurerm_resource_group.main.name
   env                 = var.env
