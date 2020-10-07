f853cfb6adc3635ff0c9e78a277230351efb79aa
stravze <stravze@gmail.com>
2020-10-07 16:18:10 +0100

    adding missing zone info

--- components/prod/dns_zone.tf
+++ components/prod/dns_zone.tf
@@ -52,6 +52,11 @@ resource "azurerm_dns_zone" "hearings" {
   resource_group_name = data.azurerm_resource_group.main.name
 }
 
+resource "azurerm_dns_zone" "vh" {
+  name                = "vh.hmcts.net"
+  resource_group_name = data.azurerm_resource_group.main.name
+}
+
 resource "azurerm_dns_zone" "reform-hmcts" {
   name                = "reform.hmcts.net"
   resource_group_name = data.azurerm_resource_group.main.name
