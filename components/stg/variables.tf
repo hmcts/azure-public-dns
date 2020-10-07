3917f07faa4c621cc8768760045575adc427ad9b
Kara Nottingham <45759232+karanotts@users.noreply.github.com>
2020-10-07 07:32:46 +0100

    Remove dev test stg (#311)
    * add dev zone
    
    * add test zone
    
    * fmt tf
    
    * add stg zone
    
    * rm dev zone
    
    * add tf dev plan&apply
    
    * add dev
    
    * add dev
    
    * removing zones that will not be used
    
    Co-authored-by: Zoë <54357991+zc-hmcts@users.noreply.github.com>

--- components/stg/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "resource_group_name" {
-  type        = string
-  description = "The name of the resource group that contains the DNS zone where the records will be added."
-  default     = "reformmgmtrg"
-}
