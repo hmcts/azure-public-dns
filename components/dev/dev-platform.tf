4442d07586b9d7f8ae72875c2df7c399d6cdb21f
Tim Jacomb <tim.jacomb@hmcts.net>
2020-10-22 09:24:42 +0100

    Fix rg location

--- components/dev/dev-platform.tf
+++ components/dev/dev-platform.tf
@@ -1,7 +1,3 @@
-data "azurerm_resource_group" "main" {
-  name = var.resource_group_name
-}
-
 resource "azurerm_dns_zone" "dev" {
   name                = "dev.platform.hmcts.net"
   resource_group_name = data.azurerm_resource_group.main.name
--- components/test/perftest-platform.tf
+++ components/test/perftest-platform.tf
@@ -1,7 +1,3 @@
-data "azurerm_resource_group" "main" {
-  name = var.resource_group_name
-}
-
 data "local_file" "perftest" {
   filename = "${path.cwd}/../../environments/test/perftest.yml"
 }
