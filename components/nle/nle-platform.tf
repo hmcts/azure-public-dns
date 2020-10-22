1a521ac4e394f83629d4e509a5e0f6ee1c766633
Tim Jacomb <tim.jacomb@hmcts.net>
2020-10-22 10:58:55 +0100

    Fix typo

--- components/nle/nle-platform.tf
+++ components/nle/nle-platform.tf
@@ -1,9 +1,5 @@
 // TODO move this to staging
 
-data "azurerm_resource_group" "main" {
-  name = var.resource_group_name
-}
-
 resource "azurerm_dns_zone" "zone" {
   name                = "nle.platform.hmcts.net"
   resource_group_name = data.azurerm_resource_group.main.name
