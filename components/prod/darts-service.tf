99ef87362e26c16a09aff66ce54b28aa12e0327b
Luigi Bitonti <luigi.bitonti@hmcts.net>
2020-10-29 14:03:23 +0000

    Add darts.service.justice.gov.uk

---
+++ components/prod/darts-service.tf
@@ -0,0 +1,16 @@
+resource "azurerm_dns_zone" "darts-service" {
+  name                = "darts.service.justice.gov.uk"
+  resource_group_name = data.azurerm_resource_group.main.name
+}
+
+data "local_file" "darts-service-config" {
+  filename = "${path.cwd}/../../environments/prod/darts-service-justice-gov-uk.yml"
+}
+
+module "apply-divorce" {
+  source              = "../../modules/azure-public-dns/"
+  a_records           = yamldecode(data.local_file.darts-service-config.content).a
+  zone_name           = azurerm_dns_zone.darts-service.name
+  resource_group_name = data.azurerm_resource_group.main.name
+  env                 = var.env
+}
