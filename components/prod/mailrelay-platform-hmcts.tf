8324344bf586ebb9de5122e566f08766772e4a05
Luigi Bitonti <luigi.bitonti@hmcts.net>
2021-03-08 09:43:21 +0000

    DTSPO-1477 Add mailrelay platform records

---
+++ components/prod/mailrelay-platform-hmcts.tf
@@ -0,0 +1,19 @@
+resource "azurerm_dns_zone" "mailrelay-platform-hmcts" {
+  name                = "mailrelay.platform.hmcts.net"
+  resource_group_name = data.azurerm_resource_group.main.name
+}
+
+data "local_file" "mailrelay-platform-hmcts-config" {
+  filename = "${path.cwd}/../../environments/prod/mailrelay-platform-hmcts-net.yml"
+}
+
+module "mailrelay-platform-hmcts" {
+  source              = "../../modules/azure-public-dns/"
+  cname_records       = yamldecode(data.local_file.mailrelay-platform-hmcts-config.content).cname
+  ns_recordsets       = yamldecode(data.local_file.mailrelay-platform-hmcts-config.content).ns
+  a_recordsets        = yamldecode(data.local_file.mailrelay-platform-hmcts-config.content).A
+  txt_recordsets      = yamldecode(data.local_file.mailrelay-platform-hmcts-config.content).txt
+  zone_name           = azurerm_dns_zone.mailrelay-platform-hmcts.name
+  resource_group_name = data.azurerm_resource_group.main.name
+  env                 = var.env
+}
