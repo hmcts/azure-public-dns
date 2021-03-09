9a8b9e15ff6317767cf8889e343be9e788beb7d5
jordankainos <j.brown@kainos.com>
2021-03-09 09:10:09 +0000

    Adding A Records
    Adding the A record variable to nle-hmcts-net and prod-hmcts-net

--- components/prod/prod-hmcts-net.tf
+++ components/prod/prod-hmcts-net.tf
@@ -11,6 +11,7 @@ module "prod-hmcts-net" {
   source              = "../../modules/azure-public-dns/"
   cname_records       = yamldecode(data.local_file.prod-hmcts-net.content).cname
   txt_recordsets      = yamldecode(data.local_file.prod-hmcts-net.content).txt
+  a_recordsets        = yamldecode(data.local_file.prod-hmcts-net.content).A
   zone_name           = azurerm_dns_zone.prod-hmcts-net.name
   resource_group_name = data.azurerm_resource_group.main.name
   env                 = var.env
