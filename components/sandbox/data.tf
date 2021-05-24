a6f17fe56a05363e93e320ede222fcc57394c1bb
mokainos <72137340+mokainos@users.noreply.github.com>
2021-05-24 10:14:33 +0100

    modified sbox env

--- components/sandbox/data.tf
+++ components/sandbox/data.tf
@@ -3,5 +3,5 @@ data "azurerm_resource_group" "main" {
 }
 
 data "azuread_group" "dnszone_contributor_group"{
-  display_name     = "DTS Public DNS Contributor (env:${var.env})
+  display_name     = "DTS Public DNS Contributor (env:${var.env})"
 }
