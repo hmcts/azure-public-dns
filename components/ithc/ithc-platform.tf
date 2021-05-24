ed44266cacf79440b2fe36af021afeb94ab24623
mokainos <72137340+mokainos@users.noreply.github.com>
2021-05-24 12:06:33 +0100

    updated ithc dev & nle

--- components/ithc/ithc-platform.tf
+++ components/ithc/ithc-platform.tf
@@ -16,7 +16,7 @@ module "public-dns" {
   txt_recordsets      = yamldecode(data.local_file.ithc.content).txt
   ns_recordsets       = yamldecode(data.local_file.ithc.content).ns
   srv_recordsets      = yamldecode(data.local_file.ithc.content).srv
-  zone_name           = azurerm_dns_zone.zone.name
+  zone_name           = "ithc.platform.hmcts.net"
   resource_group_name = data.azurerm_resource_group.main.name
   env                 = "ithc"
 }
--- components/nle/nle-hmcts-net.tf
+++ components/nle/nle-hmcts-net.tf
@@ -1,8 +1,3 @@
-resource "azurerm_dns_zone" "nle-hmcts-net" {
-  name                = "nle.hmcts.net"
-  resource_group_name = data.azurerm_resource_group.main.name
-}
-
 data "local_file" "nle-hmcts-net" {
   filename = "${path.cwd}/../../environments/nle/nle-hmcts-net.yml"
 }
@@ -12,7 +7,7 @@ module "nle-hmcts-net" {
   cname_records       = yamldecode(data.local_file.nle-hmcts-net.content).cname
   txt_recordsets      = yamldecode(data.local_file.nle-hmcts-net.content).txt
   a_recordsets        = yamldecode(data.local_file.nle-hmcts-net.content).A
-  zone_name           = azurerm_dns_zone.nle-hmcts-net.name
+  zone_name           = "nle.hmcts.net"
   resource_group_name = data.azurerm_resource_group.main.name
   env                 = var.env
 }
