5feda0c3e0e4faf2ef03f3a332e45256e98a470b
makowalski <marcin.kowalski.it.consulting@gmail.com>
2021-07-27 10:32:17 +0100

    removing txt mapping for AIP

--- components/prod/appeal-immigration.tf
+++ components/prod/appeal-immigration.tf
@@ -5,7 +5,6 @@ data "local_file" "appeal-immigration-asylum-decision-config" {
 module "appeal-immigration" {
   source              = "../../modules/azure-public-dns/"
   cname_records       = yamldecode(data.local_file.appeal-immigration-asylum-decision-config.content).cname
-  txt_recordsets      = yamldecode(data.local_file.appeal-immigration-asylum-decision-config.content).txt
   zone_name           = "appeal-immigration-asylum-decision.service.gov.uk"
   resource_group_name = data.azurerm_resource_group.main.name
   env                 = var.env
