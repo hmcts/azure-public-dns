c942b6bba1e4d9843f5cfb17c7b5a767aa80b9b1
msl8r <72858053+msl8r@users.noreply.github.com>
2021-08-11 15:35:06 +0100

    DTSPO-4363 - fix justice

--- components/prod/juror-bureau.tf
+++ components/prod/juror-bureau.tf
@@ -1,12 +1,12 @@
 
 data "local_file" "juror-bureau-config" {
-  filename = "${path.cwd}/../../environments/prod/juror-bureau-service-gov-uk.yml"
+  filename = "${path.cwd}/../../environments/prod/juror-bureau-justice-gov-uk.yml"
 }
 
 module "juror-bureau" {
   source              = "../../modules/azure-public-dns/"
   cname_records       = yamldecode(data.local_file.juror-bureau-config.content).cname
-  zone_name           = "juror-bureau.service.gov.uk"
+  zone_name           = "juror-bureau.justice.gov.uk"
   resource_group_name = data.azurerm_resource_group.main.name
   env                 = var.env
 }
