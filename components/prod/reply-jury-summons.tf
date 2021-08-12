5a40bb1099e8a25e3f45cffed34afe8d7f4dda73
msl8r <72858053+msl8r@users.noreply.github.com>
2021-08-12 10:27:13 +0100

    DTSPO-4363 - enable A record management and add existing records

--- components/prod/reply-jury-summons.tf
+++ components/prod/reply-jury-summons.tf
@@ -6,6 +6,7 @@ data "local_file" "reply-jury-summons-config" {
 module "reply-jury-summons" {
   source              = "../../modules/azure-public-dns/"
   cname_records       = yamldecode(data.local_file.reply-jury-summons-config.content).cname
+  a_recordsets        = yamldecode(data.local_file.reply-jury-summons-config.content).A
   zone_name           = "reply-jury-summons.service.gov.uk"
   resource_group_name = data.azurerm_resource_group.main.name
   env                 = var.env
