84f148573867b4146c9e1477bf83e3af672df910
msl8r <72858053+msl8r@users.noreply.github.com>
2021-08-12 10:56:55 +0100

    DTSPO-4363 jd prod fd (#628)
    * DTSPO-4363 - add jd apps frontdoor records
    
    * DTSPO-4363 - fix module name
    
    * DTSPO-4363 - fix fd name
    
    * DTSPO-4363 - fix justice
    
    * DTSPO-4363 - enable A record management and add existing records
    
    * DTSPO-4363 - re-enable prod apply
    
    * Update environments/prod/reply-jury-summons-service-gov-uk.yml
    
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>
    
    * Update environments/prod/juror-bureau-justice-gov-uk.yml
    
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>
    
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>

--- components/prod/juror-bureau.tf
+++ components/prod/juror-bureau.tf
@@ -6,6 +6,7 @@ data "local_file" "juror-bureau-config" {
 module "juror-bureau" {
   source              = "../../modules/azure-public-dns/"
   cname_records       = yamldecode(data.local_file.juror-bureau-config.content).cname
+  a_recordsets        = yamldecode(data.local_file.juror-bureau-config.content).A
   zone_name           = "juror-bureau.justice.gov.uk"
   resource_group_name = data.azurerm_resource_group.main.name
   env                 = var.env
