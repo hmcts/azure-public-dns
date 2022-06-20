509fdd1d22c395c1b7bf263ffb487e8bc9f38700
Zoe Cope <zoe.cope@hmcts.net>
2022-06-20 10:01:56 +0100

    sbox provider

--- components/sandbox/provider.tf
+++ components/sandbox/provider.tf
@@ -1,13 +1,9 @@
 provider "azurerm" {
+  version = "=1.44.0"
   features {}
   skip_provider_registration = true
 }
 
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.10.0"
-    }
-  }
+  backend "azurerm" {}
 }
