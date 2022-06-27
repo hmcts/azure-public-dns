443ad6c55ffd3045ffcd557aa4845eab8705ffaa
Mark Butler <42868152+mark-butler-solirius@users.noreply.github.com>
2022-06-27 19:19:02 +0100

    Add sendgrid.net email authentication cname records for tribunalsdecisions legacy website (#898)
    Add the missing staging dns records

--- components/staging/dts-legacy-apps.tf
+++ components/staging/dts-legacy-apps.tf
@@ -37,3 +37,12 @@ module "immigrationappealsonline" {
   resource_group_name = data.azurerm_resource_group.main.name
   env                 = "stg"
 }
+
+module "tribunalsdecisions" {
+  source              = "../../modules/azure-public-dns/"
+  cname_records       = yamldecode(data.local_file.staging-config.content).cname
+  a_recordsets        = yamldecode(data.local_file.staging-config.content).A
+  zone_name           = "staging.tribunalsdecisions.service.gov.uk"
+  resource_group_name = data.azurerm_resource_group.main.name
+  env                 = "stg"
+}
