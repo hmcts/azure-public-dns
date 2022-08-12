518477bf2053d18615b12136297ec485fa189357
Christopher Pateman <christopher.pateman@version1.com>
2022-08-12 11:10:11 +0100

    VIH-9224 - Add missing namespace option

--- components/prod/hearings.tf
+++ components/prod/hearings.tf
@@ -4,6 +4,7 @@ data "local_file" "hearings-config" {
 
 module "hearings" {
   source              = "../../modules/azure-public-dns/"
+  ns_recordsets       = yamldecode(data.local_file.hearings-config.content).ns
   cname_records       = yamldecode(data.local_file.hearings-config.content).cname
   a_recordsets        = yamldecode(data.local_file.hearings-config.content).A
   srv_recordsets      = yamldecode(data.local_file.hearings-config.content).srv
