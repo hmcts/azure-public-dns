10b89a7720a23af66fe669b0d45665f127e42495
Will <wrftaylor@gmail.com>
2022-08-22 09:42:29 +0100

    Update components/prod/c100-application.tf
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>

--- components/prod/c100-application.tf
+++ components/prod/c100-application.tf
@@ -6,7 +6,7 @@ module "c100-application" {
   source              = "../../modules/azure-public-dns/"
   cname_records       = yamldecode(data.local_file.c100-application-config.content).cname
   txt_recordsets      = yamldecode(data.local_file.c100-application-config.content).txt
-  zone_name           = "c100-application.platform.hmcts.net"
+  zone_name           = "apply-to-court-about-child-arrangements.service.justice.gov.uk"
   resource_group_name = data.azurerm_resource_group.main.name
   env                 = var.env
 }
