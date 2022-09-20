d43ba9563a98fead1cbc080d255f1ea1ce422a9b
Peter Moores <peter.moores@version1.com>
2022-09-20 16:42:30 +0100

    Updated to include split of 2 new domains for "claim" and "respond".

---
+++ components/prod/claim-employment-tribunals.tf
@@ -0,0 +1,12 @@
+data "local_file" "claim-et-sya-config" {
+  filename = "${path.cwd}/../../environments/prod/claim-et-sya-hmcts-net.yml"
+}
+
+module "claim-employment-tribunals" {
+  source              = "../../modules/azure-public-dns/"
+  cname_records       = yamldecode(data.local_file.claim-et-sya-config.content).cname
+  a_recordsets        = yamldecode(data.local_file.claim-et-sya-config.content).A
+  zone_name           = "claim-employment-tribunals.service.gov.uk"
+  resource_group_name = data.azurerm_resource_group.main.name
+  env                 = var.env
+}
---
+++ components/prod/respond-employment-tribunals.tf
@@ -0,0 +1,12 @@
+data "local_file" "claim-et-ra-config" {
+  filename = "${path.cwd}/../../environments/prod/claim-et-ra-hmcts-net.yml"
+}
+
+module "respond-employment-tribunals" {
+  source              = "../../modules/azure-public-dns/"
+  cname_records       = yamldecode(data.local_file.claim-et-ra-config.content).cname
+  a_recordsets        = yamldecode(data.local_file.claim-et-ra-config.content).A
+  zone_name           = "respond-employment-tribunals.service.gov.uk"
+  resource_group_name = data.azurerm_resource_group.main.name
+  env                 = var.env
+}
