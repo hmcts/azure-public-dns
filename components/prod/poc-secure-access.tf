1af79917e7c0475c9251a751a1d87c3417a34f70
taherkhan30 <taher.khan@hmcts.net>
2022-12-01 11:28:14 +0000

    updated module name

--- components/prod/poc-secure-access.tf
+++ components/prod/poc-secure-access.tf
@@ -2,7 +2,7 @@ data "local_file" "poc-secure-access-config" {
   filename = "${path.cwd}/../../environments/prod/poc-secureaccess-justice-gov-uk.yml"
 }
 
-module "secure-access" {
+module "poc-secure-access" {
   source              = "../../modules/azure-public-dns/"
   cname_records       = yamldecode(data.local_file.poc-secure-access-config.content).ns
   a_recordsets        = yamldecode(data.local_file.poc-secure-access-config.content).A
