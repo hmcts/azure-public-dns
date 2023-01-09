1a785dc72d87a87a1fc66042b1edcfa6b5e36041
Aloknath Datta <alokdatta.uit@gmail.com>
2023-01-09 10:40:10 +0000

    Modified the TF changes as suggested

--- components/prod/childarrangement-familyinjunction.tf
+++ components/prod/childarrangement-familyinjunction.tf
@@ -5,7 +5,10 @@ data "local_file" "childarrangement-familyinjunction-config" {
 module "childarrangement-familyinjunction" {
   source              = "../../modules/azure-public-dns/"
   cname_records       = yamldecode(data.local_file.childarrangement-familyinjunction-config.content).cname
-  zone_name           = "childarrangement-familyinjunction.service.gov.uk"
+  ns_recordsets       = yamldecode(data.local_file.childarrangement-familyinjunction-config.content).ns
+  a_recordsets        = yamldecode(data.local_file.childarrangement-familyinjunction-config.content).A
+  txt_recordsets      = yamldecode(data.local_file.childarrangement-familyinjunction-config.content).txt
+  mx_recordsets       = yamldecode(data.local_file.childarrangement-familyinjunction-config.content).mx
   resource_group_name = data.azurerm_resource_group.main.name
   env                 = var.env
 }
