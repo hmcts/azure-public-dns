f41a886a7aaa42de0a8ac0e65ebd8c1291cb4e49
Chirag Pareek <111435448+cpareek@users.noreply.github.com>
2023-03-24 14:55:58 +0000

    deploying two public zones with no records (#1118)
    * deploying two public zones with no records
    
    * fixed errrors

---
+++ components/prod/prod-internal-hmcts.tf
@@ -0,0 +1,12 @@
+data "local_file" "prod-internal-hmcts-config" {
+  filename = "${path.cwd}/../../environments/prod/prod-internal-hmcts-net.yml"
+}
+
+module "prod-internal" {
+  source              = "../../modules/azure-public-dns/"
+  cname_records       = yamldecode(data.local_file.prod-internal-hmcts-config.content).cname
+  a_recordsets        = yamldecode(data.local_file.prod-internal-hmcts-config.content).A
+  zone_name           = "prod.internal.hmcts.net"
+  resource_group_name = data.azurerm_resource_group.main.name
+  env                 = var.env
+}
