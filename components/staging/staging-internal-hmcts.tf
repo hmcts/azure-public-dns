4befa13e73936d2c11b4ab83fb05b75aabb57f3c
Chirag Pareek <chirag.pareek@justice.gov.uk>
2023-03-24 13:34:26 +0000

    deploying two public zones with no records

---
+++ components/staging/staging-internal-hmcts.tf
@@ -0,0 +1,13 @@
+data "local_file" "staging-internal-hmcts-config" {
+  filename = "${path.cwd}/../../environments/staging/staging-internal-hmcts-net.yml"
+}
+
+module "staging-internal" {
+  source              = "../../modules/azure-public-dns/"
+  cname_records       = yamldecode(data.local_file.staging-internal-hmcts-config.content).cname
+  a_recordsets        = yamldecode(data.local_file.staging-internal-hmcts-config.content).A
+  srv_recordsets      = yamldecode(data.local_file.staging-internal-hmcts-config.content).srv
+  zone_name           = "staging.internal.hmcts.net"
+  resource_group_name = data.azurerm_resource_group.main.name
+  env                 = "stg"
+}
