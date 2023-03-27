bb0babe22b98fb1621f72729427dce536a822d33
Chirag Pareek <111435448+cpareek@users.noreply.github.com>
2023-03-27 09:13:03 +0100

    in order to fix the ns lookup issue (#1120)

--- components/prod/prod-internal-hmcts.tf
+++ components/prod/prod-internal-hmcts.tf
@@ -6,6 +6,7 @@ module "prod-internal" {
   source              = "../../modules/azure-public-dns/"
   cname_records       = yamldecode(data.local_file.prod-internal-hmcts-config.content).cname
   a_recordsets        = yamldecode(data.local_file.prod-internal-hmcts-config.content).A
+  ns_recordsets       = yamldecode(data.local_file.prod-internal-hmcts-config.content).ns
   zone_name           = "prod.internal.hmcts.net"
   resource_group_name = data.azurerm_resource_group.main.name
   env                 = var.env
