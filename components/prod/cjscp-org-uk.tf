688e5236e87759b8a8a89b16d5a59d6c692bcc47
Tim Jacomb <21194782+timja@users.noreply.github.com>
2023-03-27 15:03:31 +0100

    Add hmcts.net, cjscp and ejudiciary.net (#1127)

---
+++ components/prod/cjscp-org-uk.tf
@@ -0,0 +1,15 @@
+data "local_file" "cjscp_org_uk_config" {
+  filename = "${path.cwd}/../../environments/prod/cjscp-org-uk.yml"
+}
+
+module "cjscp_org_uk" {
+  source              = "../../modules/azure-public-dns/"
+  a_recordsets        = yamldecode(data.local_file.cjscp_org_uk_config.content).A
+  cname_records       = yamldecode(data.local_file.cjscp_org_uk_config.content).cname
+  mx_recordsets       = yamldecode(data.local_file.cjscp_org_uk_config.content).mx
+  ns_recordsets       = yamldecode(data.local_file.cjscp_org_uk_config.content).ns
+  txt_recordsets      = yamldecode(data.local_file.cjscp_org_uk_config.content).txt
+  zone_name           = "cjscp.org.uk"
+  resource_group_name = data.azurerm_resource_group.main.name
+  env                 = var.env
+}
