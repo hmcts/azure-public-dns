a27534c5558ce4edd5e53ad3a6d418adafb5ef54
Tim Jacomb <timjacomb1@gmail.com>
2023-03-30 15:30:16 +0100

    terraform fmt

--- components/prod/hmcts-net.tf
+++ components/prod/hmcts-net.tf
@@ -9,7 +9,7 @@ module "hmcts_net" {
   a_recordsets        = yamldecode(data.local_file.hmcts_net_config.content).A
   txt_recordsets      = yamldecode(data.local_file.hmcts_net_config.content).txt
   mx_recordsets       = yamldecode(data.local_file.hmcts_net_config.content).mx
-  srv_recordsets       = yamldecode(data.local_file.hmcts_net_config.content).srv
+  srv_recordsets      = yamldecode(data.local_file.hmcts_net_config.content).srv
   zone_name           = "hmcts.net"
   resource_group_name = data.azurerm_resource_group.main.name
   env                 = var.env
