d7edd423bf8a01a608401d7bb47ec23099ab263f
Tim Jacomb <timjacomb1@gmail.com>
2023-03-31 08:59:04 +0100

    Disable hmcts.net for now

--- components/prod/hmcts-net.tf
+++ components/prod/hmcts-net.tf
@@ -2,15 +2,16 @@ data "local_file" "hmcts_net_config" {
   filename = "${path.cwd}/../../environments/prod/hmcts-net.yml"
 }
 
-module "hmcts_net" {
-  source              = "../../modules/azure-public-dns/"
-  cname_records       = yamldecode(data.local_file.hmcts_net_config.content).cname
-  ns_recordsets       = yamldecode(data.local_file.hmcts_net_config.content).ns
-  a_recordsets        = yamldecode(data.local_file.hmcts_net_config.content).A
-  txt_recordsets      = yamldecode(data.local_file.hmcts_net_config.content).txt
-  mx_recordsets       = yamldecode(data.local_file.hmcts_net_config.content).mx
-  srv_recordsets      = yamldecode(data.local_file.hmcts_net_config.content).srv
-  zone_name           = "hmcts.net"
-  resource_group_name = data.azurerm_resource_group.main.name
-  env                 = var.env
-}
+# Disabled until Azure support sort out creating this zone
+#module "hmcts_net" {
+#  source              = "../../modules/azure-public-dns/"
+#  cname_records       = yamldecode(data.local_file.hmcts_net_config.content).cname
+#  ns_recordsets       = yamldecode(data.local_file.hmcts_net_config.content).ns
+#  a_recordsets        = yamldecode(data.local_file.hmcts_net_config.content).A
+#  txt_recordsets      = yamldecode(data.local_file.hmcts_net_config.content).txt
+#  mx_recordsets       = yamldecode(data.local_file.hmcts_net_config.content).mx
+#  srv_recordsets      = yamldecode(data.local_file.hmcts_net_config.content).srv
+#  zone_name           = "hmcts.net"
+#  resource_group_name = data.azurerm_resource_group.main.name
+#  env                 = var.env
+#}
