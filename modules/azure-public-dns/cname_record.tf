1960c7d8dd9134a6e7fa609f7409bd9cfd264249
mokainos <72137340+mokainos@users.noreply.github.com>
2023-07-24 07:39:11 +0100

    DO NOT MERGE : Shutter static webapp (#1287)
    * webapp
    
    * adddomain
    
    * Update cname_record.tf
    
    * Update environments/sandbox.yml
    
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>
    
    * Update environments/sandbox.yml
    
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>
    
    * Update modules/azure-public-dns/cname_record.tf
    
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>

--- modules/azure-public-dns/cname_record.tf
+++ modules/azure-public-dns/cname_record.tf
@@ -12,5 +12,5 @@ resource "azurerm_dns_cname_record" "this" {
 
   name   = lower(each.value.name)
   ttl    = each.value.ttl
-  record = lookup(each.value, "shutter", false) == true ? "hmcts-${split(".", replace(join(".", [each.value.name, var.zone_name]), "www.", ""))[0]}-shutter-${var.env}.${local.shutter_domain}" : each.value.record
+  record = lookup(each.value, "shutter", false) == true ? "hmcts-${split(".", replace(join(".", [each.value.name, var.zone_name]), "www.", ""))[0]}-shutter-${var.env}.${local.shutter_domain}" : lookup(each.value, "shutter_v2", false) ? join(".", [join("-", [each.value.name, "shutter"]), var.zone_name]) : each.value.record
 }
