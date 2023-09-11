c372f420f6bc1d0def6190ebb96f8d7e78cd0b36
Tim Jacomb <21194782+timja@users.noreply.github.com>
2023-09-11 13:04:53 +0100

    Add more record types to hwf

--- components/prod/helpwithcourtfees.tf
+++ components/prod/helpwithcourtfees.tf
@@ -5,6 +5,8 @@ data "local_file" "hwf-pet-config" {
 module "helpwithcourtfees" {
   source              = "../../modules/azure-public-dns/"
   cname_records       = yamldecode(data.local_file.hwf-pet-config.content).cname
+  txt_records       = yamldecode(data.local_file.hwf-pet-config.content).txt
+  a_records       = yamldecode(data.local_file.hwf-pet-config.content).A
   zone_name           = "helpwithcourtfees.service.gov.uk"
   resource_group_name = data.azurerm_resource_group.main.name
   env                 = var.env
