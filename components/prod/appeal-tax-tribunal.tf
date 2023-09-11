f89b9ef8c81d31829c8a25d2f691d1d27ca62bb1
Ben Neill <ben.neill@justice.gov.uk>
2023-09-11 13:15:13 +0100

    Update appeal-tax-tribunal.tf

--- components/prod/appeal-tax-tribunal.tf
+++ components/prod/appeal-tax-tribunal.tf
@@ -5,6 +5,8 @@ data "local_file" "tt-pet-config" {
 module "appeal-tax-tribunal" {
   source              = "../../modules/azure-public-dns/"
   cname_records       = yamldecode(data.local_file.tt-pet-config.content).cname
+  txt_recordsets      = yamldecode(data.local_file.tt-pet-config.content).txt
+  a_recordsets        = yamldecode(data.local_file.tt-pet-config.content).A
   zone_name           = "appeal-tax-tribunal.service.gov.uk"
   resource_group_name = data.azurerm_resource_group.main.name
   env                 = var.env
