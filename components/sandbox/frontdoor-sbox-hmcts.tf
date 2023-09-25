8aa82f0fceee61f60313982e00870d0ecd917309
Chirag Pareek <chirag.pareek@justice.gov.uk>
2023-09-25 10:58:04 +0100

    updated zone name

--- components/sandbox/frontdoor-sbox-hmcts.tf
+++ components/sandbox/frontdoor-sbox-hmcts.tf
@@ -7,7 +7,7 @@ module "prod-internal" {
   cname_records       = yamldecode(data.local_file.frontdoor-sbox-hmcts-config.content).cname
   a_recordsets        = yamldecode(data.local_file.frontdoor-sbox-hmcts-config.content).A
   ns_recordsets       = yamldecode(data.local_file.frontdoor-sbox-hmcts-config.content).ns
-  zone_name           = "frontdoor.sandbox.hmcts.net"
+  zone_name           = "frontdoor.sandbox.platform.hmcts.net"
   resource_group_name = data.azurerm_resource_group.main.name
   env                 = "sbox"
 }
