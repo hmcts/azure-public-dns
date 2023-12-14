842ea3a4cccde9a8443ec9e5662795b05f2a4830
Ben Neill <ben.neill@justice.gov.uk>
2023-12-14 14:57:28 +0000

    Update special-tribunals.tf

--- components/prod/special-tribunals.tf
+++ components/prod/special-tribunals.tf
@@ -4,6 +4,8 @@ data "local_file" "special-tribunals-config" {
 
 module "special-tribunals" {
   source              = "../../modules/azure-public-dns/"
+  txt_recordsets      = yamldecode(data.local_file.special-tribunals-config.content).txt
+  a_recordsets        = yamldecode(data.local_file.special-tribunals-config.content).A
   cname_records       = yamldecode(data.local_file.special-tribunals-config.content).cname
   zone_name           = "special-tribunals.service.gov.uk"
   resource_group_name = data.azurerm_resource_group.main.name
