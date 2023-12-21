8448d334701d6488f4374505dd03347e7c665c18
Astha Malviya <astha.malviya@HMCTS.NET>
2023-12-21 10:03:34 +0000

    shutter money claims 1 too

---
+++ components/prod/moneyclaims1.tf
@@ -0,0 +1,11 @@
+data "local_file" "moneyclaims1-config" {
+  filename = "${path.cwd}/../../environments/prod/moneyclaims1-service-gov-uk.yml"
+}
+
+module "moneyclaims" {
+  source              = "../../modules/azure-public-dns/"
+  cname_records       = yamldecode(data.local_file.moneyclaims1-config.content).cname
+  zone_name           = "moneyclaims1.service.gov.uk"
+  resource_group_name = data.azurerm_resource_group.main.name
+  env                 = var.env
+}
