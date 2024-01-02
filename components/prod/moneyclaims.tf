75d28f1115b8201938fa7eec4e75346321dd1f62
Astha Malviya <astha.malviya@HMCTS.NET>
2024-01-02 11:45:16 +0000

    txt_recordsets and a_recordsets added

--- components/prod/moneyclaims.tf
+++ components/prod/moneyclaims.tf
@@ -5,6 +5,8 @@ data "local_file" "moneyclaims-config" {
 module "moneyclaims" {
   source              = "../../modules/azure-public-dns/"
   cname_records       = yamldecode(data.local_file.moneyclaims-config.content).cname
+  txt_recordsets      = yamldecode(data.local_file.moneyclaims-config.content).txt
+  a_recordsets        = yamldecode(data.local_file.moneyclaims-config.content).A
   zone_name           = "moneyclaims.service.gov.uk"
   resource_group_name = data.azurerm_resource_group.main.name
   env                 = var.env
--- components/prod/moneyclaims1.tf
+++ components/prod/moneyclaims1.tf
@@ -5,6 +5,8 @@ data "local_file" "moneyclaims1-config" {
 module "moneyclaims1" {
   source              = "../../modules/azure-public-dns/"
   cname_records       = yamldecode(data.local_file.moneyclaims1-config.content).cname
+  txt_recordsets      = yamldecode(data.local_file.moneyclaims1-config.content).txt
+  a_recordsets        = yamldecode(data.local_file.moneyclaims1-config.content).A
   zone_name           = "moneyclaims1.service.gov.uk"
   resource_group_name = data.azurerm_resource_group.main.name
   env                 = var.env
