be175eb42e7bf07f47e407dd85980c9401cf2a8f
Astha Malviya <astha.malviya@HMCTS.NET>
2024-01-02 09:06:02 +0000

    Changed module name to "moneyclaims1"

--- components/prod/moneyclaims1.tf
+++ components/prod/moneyclaims1.tf
@@ -2,7 +2,7 @@ data "local_file" "moneyclaims1-config" {
   filename = "${path.cwd}/../../environments/prod/moneyclaims1-service-gov-uk.yml"
 }
 
-module "moneyclaims" {
+module "moneyclaims1" {
   source              = "../../modules/azure-public-dns/"
   cname_records       = yamldecode(data.local_file.moneyclaims1-config.content).cname
   zone_name           = "moneyclaims1.service.gov.uk"
