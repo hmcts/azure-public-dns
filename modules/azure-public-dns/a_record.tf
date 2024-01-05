6efd7daf3764fe4beb0eba19b5d10c9f452be398
Alex Bance <Alex.Bance@justice.gov.uk>
2024-01-05 12:05:29 +0000

    feat: enable shuttering of APEX domains & fix child adoption shuttering

--- modules/azure-public-dns/a_record.tf
+++ modules/azure-public-dns/a_record.tf
@@ -4,12 +4,11 @@ resource "azurerm_dns_a_record" "this" {
     record.name => record
   }
 
-
   resource_group_name = lower(var.resource_group_name)
   zone_name           = azurerm_dns_zone.zone.name
 
   name               = lower(each.value.name)
   ttl                = each.value.ttl
-  records            = length(lookup(each.value, "record", [])) == 0 ? null : each.value.record
-  target_resource_id = length(lookup(each.value, "record", [])) == 0 ? each.value.alias_target_resource_id : null
+  records            = lookup(each.value, "shutter", false) == true ? null : length(lookup(each.value, "record", [])) == 0 ? null : each.value.record
+  target_resource_id = lookup(each.value, "shutter", false) == true ? each.value.shuter_resource_id : length(lookup(each.value, "record", [])) == 0 ? each.value.alias_target_resource_id : null
 }
