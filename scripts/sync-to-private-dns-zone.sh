ec7c84f34449d9dae6a5c9b6efd7885ac74a2f2a
Chirag Pareek <chirag.pareek@justice.gov.uk>
2024-02-28 11:34:08 +0000

    multi zone with multi files

--- scripts/sync-to-private-dns-zone.sh
+++ scripts/sync-to-private-dns-zone.sh
@@ -21,67 +21,72 @@ json_convert=$(yq eval -o=json "$filename")
 yaml_names=$(echo "$json_convert" | jq -c '.cname[]')
 
 for zoneName in $zones; do
-
-    # Retrieve CNAME records from public DNS zone
-    publicRecords=$(az network dns record-set list --zone-name $zoneName -g $publicZoneResourceGroup --subscription $publicZoneSubscription --query "[?contains(type,'CNAME')].{Name:name, Type:type, TTL:ttl, CNAMERecord:CNAMERecord.cname}")
-
-    privateZoneId=$(az network private-dns zone show -g $privateZoneResourceGroup -n $zoneName --query id -o tsv --subscription $privateZoneSubscription)
-
-    # Retrieve existing CNAME records from private DNS zone
-    existingPrivateRecords=$(az network private-dns record-set list --zone-name $zoneName -g $privateZoneResourceGroup --subscription $privateZoneSubscription --query "[?contains(type,'CNAME')].[name]" -o tsv)
-
-    # Loop through public DNS records and create corresponding private DNS records if they don't exist
-
-    # for record in $(echo "$publicRecords" | jq -r '.[] | @base64'); do
-    #     _jq() {
-    #     echo ${record} | base64 --decode | jq -r ${1}
-    #     }
-    #     recordName=$(_jq '.Name')
-    #     recordTTL=$(_jq '.TTL')
-    #     recordValue=$(_jq '.CNAMERecord')
-    #     echo $recordValue
-
-    #     # Check if the record already exists in private zone
-    #     # if ! az network private-dns record-set cname list --zone-name $zoneName -g $privateZoneResourceGroup --subscription $privateZoneSubscription --query "[?name=='$recordName'].name" | grep -q "$recordName"; then
-    #     if ! echo "$existingPrivateRecords" | grep -q "$recordName"; then
-    #         # Create the record in private zone
-    #         az network private-dns record-set cname create -g $privateZoneResourceGroup -z $zoneName  -n "$recordName" --subscription $privateZoneSubscription
-    #         az network private-dns record-set cname set-record --record-set-name "$recordName" -g $privateZoneResourceGroup --zone-name $zoneName --cname $recordValue  --subscription $privateZoneSubscription
-    #         echo "Created record $recordName in private zone."
-    #     else
-    #         echo "Record $recordName already exists in private zone. Skipping..."
-    #     fi
-    # done
-
-    ##### END OF LOOP #####
-
-
-    ##### NEW LOOP #####
-
-
-
-    # echo $yaml_names
-
-    while IFS= read -r entry; do
-        # Extract values from each entry
-        recordName=$(echo "$entry" | jq -r '.name')
-        recordValue=$(echo "$entry" | jq -r '.record')
-
-        syncPrivateDNS=$(echo "$entry" | jq -r '.syncPrivateDNS')
-        if [ "$syncPrivateDNS" != "false" ]; then
-            echo $recordName;
-            if ! echo "$existingPrivateRecords" | grep -q "$recordName"; then
-                # Create the record in private zone
-                az network private-dns record-set cname create -g $privateZoneResourceGroup -z $zoneName  -n "$recordName" --subscription $privateZoneSubscription
-                az network private-dns record-set cname set-record --record-set-name "$recordName" -g $privateZoneResourceGroup --zone-name $zoneName --cname $recordValue  --subscription $privateZoneSubscription
-                echo "Created record $recordName in private zone."
-            else
-                echo "Record $recordName already exists in private zone. Skipping..."
-            fi
-        fi
-        
-    done <<< "$yaml_names"
-
-    ##### END OF NEW LOOP #####
-    
+ echo "zoneName: $zoneName"
 done
+
+
+# for zoneName in $zones; do
+
+#     # Retrieve CNAME records from public DNS zone
+#     publicRecords=$(az network dns record-set list --zone-name $zoneName -g $publicZoneResourceGroup --subscription $publicZoneSubscription --query "[?contains(type,'CNAME')].{Name:name, Type:type, TTL:ttl, CNAMERecord:CNAMERecord.cname}")
+
+#     privateZoneId=$(az network private-dns zone show -g $privateZoneResourceGroup -n $zoneName --query id -o tsv --subscription $privateZoneSubscription)
+
+#     # Retrieve existing CNAME records from private DNS zone
+#     existingPrivateRecords=$(az network private-dns record-set list --zone-name $zoneName -g $privateZoneResourceGroup --subscription $privateZoneSubscription --query "[?contains(type,'CNAME')].[name]" -o tsv)
+
+#     # Loop through public DNS records and create corresponding private DNS records if they don't exist
+
+#     # for record in $(echo "$publicRecords" | jq -r '.[] | @base64'); do
+#     #     _jq() {
+#     #     echo ${record} | base64 --decode | jq -r ${1}
+#     #     }
+#     #     recordName=$(_jq '.Name')
+#     #     recordTTL=$(_jq '.TTL')
+#     #     recordValue=$(_jq '.CNAMERecord')
+#     #     echo $recordValue
+
+#     #     # Check if the record already exists in private zone
+#     #     # if ! az network private-dns record-set cname list --zone-name $zoneName -g $privateZoneResourceGroup --subscription $privateZoneSubscription --query "[?name=='$recordName'].name" | grep -q "$recordName"; then
+#     #     if ! echo "$existingPrivateRecords" | grep -q "$recordName"; then
+#     #         # Create the record in private zone
+#     #         az network private-dns record-set cname create -g $privateZoneResourceGroup -z $zoneName  -n "$recordName" --subscription $privateZoneSubscription
+#     #         az network private-dns record-set cname set-record --record-set-name "$recordName" -g $privateZoneResourceGroup --zone-name $zoneName --cname $recordValue  --subscription $privateZoneSubscription
+#     #         echo "Created record $recordName in private zone."
+#     #     else
+#     #         echo "Record $recordName already exists in private zone. Skipping..."
+#     #     fi
+#     # done
+
+#     ##### END OF LOOP #####
+
+
+#     ##### NEW LOOP #####
+
+
+
+#     # echo $yaml_names
+
+#     while IFS= read -r entry; do
+#         # Extract values from each entry
+#         recordName=$(echo "$entry" | jq -r '.name')
+#         recordValue=$(echo "$entry" | jq -r '.record')
+
+#         syncPrivateDNS=$(echo "$entry" | jq -r '.syncPrivateDNS')
+#         if [ "$syncPrivateDNS" != "false" ]; then
+#             echo $recordName;
+#             if ! echo "$existingPrivateRecords" | grep -q "$recordName"; then
+#                 # Create the record in private zone
+#                 az network private-dns record-set cname create -g $privateZoneResourceGroup -z $zoneName  -n "$recordName" --subscription $privateZoneSubscription
+#                 az network private-dns record-set cname set-record --record-set-name "$recordName" -g $privateZoneResourceGroup --zone-name $zoneName --cname $recordValue  --subscription $privateZoneSubscription
+#                 echo "Created record $recordName in private zone."
+#             else
+#                 echo "Record $recordName already exists in private zone. Skipping..."
+#             fi
+#         fi
+        
+#     done <<< "$yaml_names"
+
+#     ##### END OF NEW LOOP #####
+    
+# done
