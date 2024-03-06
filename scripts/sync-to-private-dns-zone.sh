aaf7d308a0ab72d09018ad1f2cf12f6c7e5d3512
Chirag Pareek <111435448+cpareek@users.noreply.github.com>
2024-03-06 10:59:06 +0000

    DTSPO-16109 -  Sync public cname record to private dns zone (#1627)
    * first commit - sync public to private dns zones
    
    * pipeline added
    
    * added single quote
    
    * fix
    
    * fix
    
    * fix
    
    * fix 09:49
    
    * fix 09:51
    
    * fix 09:54
    
    * fix 09:57
    
    * fix 10:03
    
    * fix 10:06
    
    * fix 10:11
    
    * fix 10:15
    
    * fix 10:31
    
    * fix 11:20
    
    * fix 11:34
    
    * fix 11:55
    
    * after adding condition for syncPrivateDNS
    
    * fix
    
    * fix 14:46
    
    * removed plumclassic2
    
    * removed plumclassic2
    
    * removed plumclassic2
    
    * last commit before removing everything
    
    * multi zone with multi files
    
    * multi zone with multi files attempt 2
    
    * attempt 3
    
    * attempt 4
    
    * attempt 5
    
    * attempt 6
    
    * attempt 7
    
    * attempt 8
    
    * attempt 9
    
    * attempt 10
    
    * attempt 11
    
    * attempt 12
    
    * attempt 13
    
    * attempt 13
    
    * attempt 14
    
    * attempt 15
    
    * attempt 16
    
    * attempt 16
    
    * attempt 17
    
    * attempt 18
    
    * attempt 19
    
    * attempt 20
    
    * updated script for today
    
    * updated script for today 1
    
    * updated script for today 2
    
    * moved yaml variable to list
    
    * testing with no syncprivatedns
    
    * testing deletion from private dns zone
    
    * testing deletion from private dns zone 1
    
    * testing addition plumclassic2
    
    * testing deletion plumclassic2
    
    * testing syncPrivateDNS
    
    * delete
    
    * remove delete
    
    * test complete delete
    
    * test with Demo environment
    
    * test with Dev environment
    
    * changed depends on
    
    * changed depends on
    
    * changed depends on
    
    * testing with ithc
    
    * testing with test and perftest
    
    * testing with staging environment
    
    * after clean up
    
    * added prod dns zones
    
    * added prod dns zones

---
+++ scripts/sync-to-private-dns-zone.sh
@@ -0,0 +1,97 @@
+#!/usr/bin/env bash
+
+
+
+publicZoneResourceGroup=$1
+publicZoneSubscription=$2
+privateZoneResourceGroup=$3
+privateZoneSubscription=$4
+zones=$5
+
+
+echo "publicZoneResourceGroup: $publicZoneResourceGroup"
+echo "publicZoneSubscription: $publicZoneSubscription"
+echo "privateZoneResourceGroup: $privateZoneResourceGroup"
+echo "privateZoneSubscription: $privateZoneSubscription"
+echo "zones: $zones"
+
+
+# Convert the input string to valid JSON
+json_string=$(echo "$zones" | jq -c '.')
+
+# Loop through each entry in the JSON
+for entry in $(echo "$json_string" | jq -c '.[]'); do
+    zoneName=$(echo "$entry" | jq -r '.dnsname')
+    filename=$(echo "$entry" | jq -r '.filename')
+    echo "SYNCING  - zone Name: $zoneName, Filename: $filename"
+
+    json_convert=$(yq eval -o=json "$filename")
+
+    yaml_names=$(echo "$json_convert" | jq -c '.cname[]')
+
+    # Retrieve CNAME records from public DNS zone
+    publicRecords=$(az network dns record-set list --zone-name $zoneName -g $publicZoneResourceGroup --subscription $publicZoneSubscription --query "[?contains(type,'CNAME')].{Name:name, Type:type, TTL:ttl, CNAMERecord:CNAMERecord.cname}")
+
+    privateZoneId=$(az network private-dns zone show -g $privateZoneResourceGroup -n $zoneName --query id -o tsv --subscription $privateZoneSubscription)
+
+    # Retrieve existing CNAME records from private DNS zone
+    existingPrivateRecords=$(az network private-dns record-set list --zone-name $zoneName -g $privateZoneResourceGroup --subscription $privateZoneSubscription --query "[?contains(type,'CNAME')].[name]" -o tsv)
+
+    
+            while IFS= read -r entry; do
+                # Extract values from each entry
+                recordName2=$(echo "$entry" | jq -r '.name')
+                syncPrivateDNS=$(echo "$entry" | jq -r '.syncPrivateDNS')
+                if [[ "$syncPrivateDNS" == "false" ]]; then
+                    echo "recordName2 $recordName2";
+                    recordPrivateDnsList+=("$recordName2")
+                fi
+                
+            done <<< "$yaml_names"
+    
+    
+    # Loop through public DNS records and create corresponding private DNS records if they don't exist
+
+
+
+    for record in $(echo "$publicRecords" | jq -r '.[] | @base64'); do
+        _jq() {
+        echo ${record} | base64 --decode | jq -r ${1}
+        }
+        recordName=$(_jq '.Name')
+        recordTTL=$(_jq '.TTL')
+        recordValue=$(_jq '.CNAMERecord')
+
+        ignore_record=false
+        echo $recordValue
+
+        for value in "${recordPrivateDnsList[@]}"
+        do 
+            if [[ $recordName == $value ]]; then
+                    ignore_record=true
+
+                # removing delete code for now
+                # if echo "$existingPrivateRecords" | grep -q "$value"; then
+                #         az network private-dns record-set cname delete -g $privateZoneResourceGroup -z $zoneName  -n "$value" --subscription $privateZoneSubscription  --yes
+                #         echo "deleted $value from $zoneName zone"
+                # fi
+            fi
+        done
+
+        if ! echo "$existingPrivateRecords" | grep -q "$recordName" && ! $ignore_record; then
+            # Create the record in private zone
+            az network private-dns record-set cname create -g $privateZoneResourceGroup -z $zoneName  -n "$recordName" --subscription $privateZoneSubscription
+            az network private-dns record-set cname set-record --record-set-name "$recordName" -g $privateZoneResourceGroup --zone-name $zoneName --cname $recordValue  --subscription $privateZoneSubscription
+            echo "Created record $recordName in private zone."
+        else
+            if $ignore_record; then
+                echo "$recordName set NOT to sync with private dns zone" 
+            else
+                echo "Record $recordName already exists in private zone. Skipping..."   
+            fi
+        fi
+    done
+
+    ##### END OF LOOP #####
+    
+done
