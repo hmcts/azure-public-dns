2a973c1683a7e5ceaa2b53b682aacf53086d2445
Marty Fox <martin.fox1@justice.gov.uk>
2024-03-11 13:50:30 +0000

    Fix sandbox component reference, add test example trues

--- components/sandbox/sandbox-platform.tf
+++ components/sandbox/sandbox-platform.tf
@@ -8,14 +8,14 @@ data "local_file" "sandbox-shutter-config" {
 
 module "public-dns" {
   source              = "../../modules/azure-public-dns/"
-  cname_records       = yamldecode(data.local_file.configuration.content).cname
-  mx_recordsets       = yamldecode(data.local_file.configuration.content).mx
-  a_recordsets        = yamldecode(data.local_file.configuration.content).A
-  txt_recordsets      = yamldecode(data.local_file.configuration.content).txt
+  cname_records       = yamldecode(data.local_file.sandbox-config.content).cname
+  mx_recordsets       = yamldecode(data.local_file.sandbox-config.content).mx
+  a_recordsets        = yamldecode(data.local_file.sandbox-config.content).A
+  txt_recordsets      = yamldecode(data.local_file.sandbox-config.content).txt
   zone_name           = "sandbox.platform.hmcts.net"
   resource_group_name = data.azurerm_resource_group.main.name
   env                 = "sbox"
-  ns_recordsets       = yamldecode(data.local_file.configuration.content).ns
-  srv_recordsets      = yamldecode(data.local_file.configuration.content).srv
+  ns_recordsets       = yamldecode(data.local_file.sandbox-config.content).ns
+  srv_recordsets      = yamldecode(data.local_file.sandbox-config.content).srv
   shutter_config      = data.local_file.sandbox-shutter-config.content
 }
