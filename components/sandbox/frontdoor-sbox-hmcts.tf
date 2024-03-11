0acfbea089a1b7782da6f541aa4189acfe263aad
Marty Fox <martin.fox1@justice.gov.uk>
2024-03-11 15:02:12 +0000

    Resource naming convention updated, removing adverify records from shuttering file

--- components/sandbox/frontdoor-sbox-hmcts.tf
+++ components/sandbox/frontdoor-sbox-hmcts.tf
@@ -1,18 +1,18 @@
-data "local_file" "frontdoor-sbox-hmcts-config" {
+data "local_file" "frontdoor_sbox_hmcts_config" {
   filename = "${path.cwd}/../../environments/sandbox/frontdoor-sbox-hmcts.yml"
 }
 
-data "local_file" "frontdoor-sbox-hmcts-shutter-config" {
+data "local_file" "frontdoor_sbox_hmcts_shutter_config" {
   filename = "${path.cwd}/../../shuttering/sandbox/frontdoor-sbox-hmcts.yml"
 }
 
 module "prod-internal" {
   source              = "../../modules/azure-public-dns/"
-  cname_records       = yamldecode(data.local_file.frontdoor-sbox-hmcts-config.content).cname
-  a_recordsets        = yamldecode(data.local_file.frontdoor-sbox-hmcts-config.content).A
-  ns_recordsets       = yamldecode(data.local_file.frontdoor-sbox-hmcts-config.content).ns
+  cname_records       = yamldecode(data.local_file.frontdoor_sbox_hmcts_config.content).cname
+  a_recordsets        = yamldecode(data.local_file.frontdoor_sbox_hmcts_config.content).A
+  ns_recordsets       = yamldecode(data.local_file.frontdoor_sbox_hmcts_config.content).ns
   zone_name           = "frontdoor.sandbox.platform.hmcts.net"
   resource_group_name = data.azurerm_resource_group.main.name
   env                 = "sbox"
-  shutter_config      = data.local_file.frontdoor-sbox-hmcts-shutter-config.content
+  shutter_config      = data.local_file.frontdoor_sbox_hmcts_shutter_config.content
 }
--- components/sandbox/sandbox-platform.tf
+++ components/sandbox/sandbox-platform.tf
@@ -1,21 +1,21 @@
-data "local_file" "sandbox-config" {
+data "local_file" "sandbox_config" {
   filename = "${path.cwd}/../../environments/sandbox/sandbox.yml"
 }
 
-data "local_file" "sandbox-shutter-config" {
+data "local_file" "sandbox_shutter_config" {
   filename = "${path.cwd}/../../shuttering/sandbox/sandbox.yml"
 }
 
 module "public-dns" {
   source              = "../../modules/azure-public-dns/"
-  cname_records       = yamldecode(data.local_file.sandbox-config.content).cname
-  mx_recordsets       = yamldecode(data.local_file.sandbox-config.content).mx
-  a_recordsets        = yamldecode(data.local_file.sandbox-config.content).A
-  txt_recordsets      = yamldecode(data.local_file.sandbox-config.content).txt
+  cname_records       = yamldecode(data.local_file.sandbox_config.content).cname
+  mx_recordsets       = yamldecode(data.local_file.sandbox_config.content).mx
+  a_recordsets        = yamldecode(data.local_file.sandbox_config.content).A
+  txt_recordsets      = yamldecode(data.local_file.sandbox_config.content).txt
   zone_name           = "sandbox.platform.hmcts.net"
   resource_group_name = data.azurerm_resource_group.main.name
   env                 = "sbox"
-  ns_recordsets       = yamldecode(data.local_file.sandbox-config.content).ns
-  srv_recordsets      = yamldecode(data.local_file.sandbox-config.content).srv
-  shutter_config      = data.local_file.sandbox-shutter-config.content
+  ns_recordsets       = yamldecode(data.local_file.sandbox_config.content).ns
+  srv_recordsets      = yamldecode(data.local_file.sandbox_config.content).srv
+  shutter_config      = data.local_file.sandbox_shutter_config.content
 }
