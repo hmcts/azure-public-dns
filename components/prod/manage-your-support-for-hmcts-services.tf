97d50ac0aef8241e236d314edd0fce48862b2910
Ieuan Byers <ieuan.byers@justice.gov.uk>
2024-03-13 10:25:24 +0000

    Fix typo

--- components/prod/manage-your-support-for-hmcts-services.tf
+++ components/prod/manage-your-support-for-hmcts-services.tf
@@ -5,7 +5,7 @@ data "local_file" "manage-your-support-for-hmcts-services-config" {
 module "manage-your-support-for-hmcts-services" {
   source              = "../../modules/azure-public-dns/"
   cname_records       = yamldecode(data.local_file.manage-your-support-for-hmcts-services-config.content).cname
-  a_recordsets        = yamldecode(data.local_file.apply-to-adopt-a-child-placed-in-your-care-config.content).A
+  a_recordsets        = yamldecode(data.local_file.manage-your-support-for-hmcts-services-config.content).A
   zone_name           = "manage-your-support-for-hmcts-services.service.gov.uk"
   resource_group_name = data.azurerm_resource_group.main.name
   env                 = var.env
