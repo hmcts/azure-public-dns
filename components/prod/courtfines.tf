779b1ebb484364f01744f75375bb0fa11c2001b1
Marty Fox <martin.fox1@justice.gov.uk>
2024-03-13 09:22:31 +0000

    Missed courtfines resource name

--- components/prod/courtfines.tf
+++ components/prod/courtfines.tf
@@ -2,7 +2,7 @@ data "local_file" "courtfines-config" {
   filename = "${path.cwd}/../../environments/prod/courtfines-direct-gov-uk.yml"
 }
 
-data "local_file" "frontdoor_sbox_hmcts_shutter_config" {
+data "local_file" "courtfines_shutter_config" {
   filename = "${path.cwd}/../../shuttering/prod/courtfines-direct-gov-uk.yml"
 }
 
@@ -13,5 +13,5 @@ module "courtfines" {
   zone_name           = "courtfines.direct.gov.uk"
   resource_group_name = data.azurerm_resource_group.main.name
   env                 = var.env
-  shutter_config      = data.local_file.frontdoor_sbox_hmcts_shutter_config.content
+  shutter_config      = data.local_file.courtfines_shutter_config.content
 }
