e8f3966894996d194d5e111877802c5d90141f8c
Ieuan Byers <ieuan.byers@justice.gov.uk>
2024-06-20 15:58:18 +0100

    Move shutter by area to single file

--- modules/azure-public-dns/locals.tf
+++ modules/azure-public-dns/locals.tf
@@ -1,15 +1,11 @@
-data "local_file" "shutter_all_cft" {
-  filename = "${path.cwd}/../../shuttering/prod/shutter_all_cft.yml"
-}
-
-data "local_file" "shutter_all_sds" {
-  filename = "${path.cwd}/../../shuttering/prod/shutter_all_sds.yml"
+data "local_file" "shutter_by_area" {
+  filename = "${path.cwd}/../../shuttering/prod/shutter_by_area.yml"
 }
 
 locals {
   // Extract global shutter value if available or false
   shutter_all = lookup(yamldecode(var.shutter_config), "shutter_all", false)
 
-  shutter_all_cft = lookup(yamldecode(data.local_file.shutter_all_cft.content), "shutter_all_cft", false)
-  shutter_all_sds = lookup(yamldecode(data.local_file.shutter_all_sds.content), "shutter_all_sds", false)
+  shutter_all_cft = lookup(yamldecode(data.local_file.shutter_by_area.content), "shutter_all_cft", false)
+  shutter_all_sds = lookup(yamldecode(data.local_file.shutter_by_area.content), "shutter_all_sds", false)
 }
