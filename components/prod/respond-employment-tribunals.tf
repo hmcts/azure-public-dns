bf7d323be4c770d17d8f04a2f553b388aeec2b81
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2024-07-17 08:43:48 +0100

    ET Respond FE (#1813)

--- components/prod/respond-employment-tribunals.tf
+++ components/prod/respond-employment-tribunals.tf
@@ -1,16 +1,16 @@
-data "local_file" "respond-et-ra-config" {
-  filename = "${path.cwd}/../../environments/prod/respond-et-ra-hmcts-net.yml"
+data "local_file" "respond-et-syr-config" {
+  filename = "${path.cwd}/../../environments/prod/respond-et-syr-hmcts-net.yml"
 }
 
-data "local_file" "respond_et_ra_shutter_config" {
-  filename = "${path.cwd}/../../shuttering/prod/respond-et-ra-hmcts-net.yml"
+data "local_file" "respond_et_syr_shutter_config" {
+  filename = "${path.cwd}/../../shuttering/prod/respond-et-syr-hmcts-net.yml"
 }
 
 module "respond-employment-tribunals" {
   source              = "../../modules/azure-public-dns/"
-  cname_records       = yamldecode(data.local_file.respond-et-ra-config.content).cname
+  cname_records       = yamldecode(data.local_file.respond-et-syr-config.content).cname
   zone_name           = "respond-employment-tribunals.service.gov.uk"
   resource_group_name = data.azurerm_resource_group.main.name
   env                 = var.env
-  shutter_config      = data.local_file.respond_et_ra_shutter_config.content
+  shutter_config      = data.local_file.respond_et_syr_shutter_config.content
 }
