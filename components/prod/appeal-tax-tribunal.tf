31cd604f44b0cb48bcc8275e5f9488b28e7bf29b
Marty Fox <martin.fox1@justice.gov.uk>
2024-07-29 14:07:48 +0100

    Updating prod to new format

--- components/prod/appeal-tax-tribunal.tf
+++ components/prod/appeal-tax-tribunal.tf
@@ -10,6 +10,6 @@ module "appeal-tax-tribunal" {
   source              = "../../modules/azure-public-dns/"
   resource_group_name = data.azurerm_resource_group.main.name
   env                 = var.env
-  dns_config          = data.local_file.tt_pet_shutter_config.content
+  dns_config          = data.local_file.tt-pet-config.content
   shutter_config      = data.local_file.tt_pet_shutter_config.content
 }
