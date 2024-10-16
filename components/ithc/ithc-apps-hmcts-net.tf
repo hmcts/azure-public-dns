d912ad154c389d9b021dfbd4b2471846e05b297b
Ieuan Byers <ieuan.byers@justice.gov.uk>
2024-10-16 14:03:07 +0100

    Change module name

--- components/ithc/ithc-apps-hmcts-net.tf
+++ components/ithc/ithc-apps-hmcts-net.tf
@@ -2,7 +2,7 @@ data "local_file" "ithc-apps-hmcts-config" {
   filename = "${path.cwd}/../../environments/ithc/ithc-apps-hmcts-net.yml"
 }
 
-module "public-dns" {
+module "ithc-apps-public-dns" {
   source              = "../../modules/azure-public-dns/"
   resource_group_name = data.azurerm_resource_group.main.name
   env                 = "ithc"
