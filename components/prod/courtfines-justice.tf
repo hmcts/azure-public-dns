de8ede805fe356500241060639e4e42ef48ab6bf
AlexDaviesCGI <99199089+alexdavies2463@users.noreply.github.com>
2026-01-27 11:15:29 +0000

    added component and shuttering config for courtfines

---
+++ components/prod/courtfines-justice.tf
@@ -0,0 +1,15 @@
+data "local_file" "courtfines-config" {
+  filename = "${path.cwd}/../../environments/prod/courtfines-justice-gov-uk.yml"
+}
+
+data "local_file" "courtfines_shutter_config" {
+  filename = "${path.cwd}/../../shuttering/prod/courtfines-justice-gov-uk.yml"
+}
+
+module "courtfines" {
+  source              = "../../modules/azure-public-dns/"
+  resource_group_name = data.azurerm_resource_group.main.name
+  env                 = var.env
+  dns_config          = data.local_file.courtfines-config.content
+  shutter_config      = data.local_file.courtfines_shutter_config.content
+}
