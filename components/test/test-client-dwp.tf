d2c5eddfd60709748c84f81a226f4a5b61a0e913
jordankainos <j.brown@kainos.com>
2026-04-13 13:33:47 +0100

    Fixing environment

--- components/test/test-client-dwp.tf
+++ components/test/test-client-dwp.tf
@@ -1,10 +1,10 @@
 data "local_file" "test-client-dwp-hmcts-config" {
-  filename = "${path.cwd}/../../environments/dev/test-client-dwp-hmcts.yml"
+  filename = "${path.cwd}/../../environments/test/test-client-dwp-hmcts.yml"
 }
 
 module "test-client-dwp" {
   source              = "../../modules/azure-public-dns/"
   resource_group_name = data.azurerm_resource_group.main.name
-  env                 = "dev"
+  env                 = "test"
   dns_config          = data.local_file.test-client-dwp-hmcts-config.content
 }
